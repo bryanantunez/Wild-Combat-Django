9a4a39f78e1a6081ea5992cfb9edb7f98e86a749
javieramoraless <javiera.morales0142@gmail.com>
2021-06-22 23:29:39 -0400

    arreglo

--- rest_gym/urls.py
+++ rest_gym/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from rest_gym.views import lista_eventos
 
 urlpatterns = [
-    path('lista_eventos',lista_eventos,name='lista_eventos');
-    
+    path('lista_eventos',lista_eventos,name='lista_eventos'),
+
 ]
