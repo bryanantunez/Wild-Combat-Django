16a9de56f1138425f0b52647793391c4f3c3e052
NicolasRoaGazul <ni.roag@duocuc.cl>
2021-06-29 14:35:25 -0400

    api /id

--- rest_gym/urls.py
+++ rest_gym/urls.py
@@ -3,5 +3,5 @@ from rest_gym.views import lista_eventos,detalle_evento
 
 urlpatterns = [
     path('lista_eventos',lista_eventos,name='lista_eventos'),
-    path('detalle_evento',detalle_evento,name='detalle_evento'),
+    path('detalle_evento/<id>',detalle_evento,name='detalle_evento'),
 ]
