c06558df5081ad1f9cd10a10adbdfa2318c4d6dd
NicolasRoaGazul <ni.roag@duocuc.cl>
2021-06-29 16:07:20 -0400

    api casi fin

--- rest_gym/urls.py
+++ rest_gym/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_gym.views import lista_eventos,detalle_evento
 
 urlpatterns = [
-    path('lista_eventos',lista_eventos,name='lista_eventos'),
-    path('detalle_evento/<id>',detalle_evento,name='detalle_evento'),
+    path('lista-eventos',lista_eventos,name='lista_eventos'),
+    path('detalle-evento/<id>',detalle_evento,name='detalle_evento'),
 ]
 
